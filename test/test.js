a37d3a7153489f0f9b166542154606c66a38a08c
Wesley Luyten <me@wesleyluyten.com>
2023-07-13 09:51:17 -0500

    test: add tests and coverage

--- test/test.js
+++ test/test.js
@@ -1,8 +1,54 @@
-import { fixture, assert } from '@open-wc/testing';
+import { fixture, assert, oneEvent, aTimeout } from '@open-wc/testing';
 import '../dist/polyfill.js';
 import { VideoTrackList } from '../dist/video-track-list.js';
+import { VideoRenditionList } from '../dist/video-rendition-list.js';
 
 it('is an instance of VideoTrackList', async function () {
   const video = await fixture(`<video></video>`);
   assert(video.videoTracks instanceof VideoTrackList);
+});
+
+it('is an instance of VideoRenditionList', async function () {
+  const video = await fixture(`<video></video>`);
+  assert(video.videoRenditions instanceof VideoRenditionList);
+});
+
+it('fires queued addtrack event', async function () {
+  const video = await fixture(`<video></video>`);
+  const track = video.addVideoTrack('main');
+  const event = await oneEvent(video.videoTracks, 'addtrack');
+  assert.equal(track, event.track);
+  assert.equal(video.videoTracks.length, 1);
+});
+
+it('fires queued removetrack event', async function () {
+  const video = await fixture(`<video></video>`);
+  const track = video.addVideoTrack('main');
+  video.removeVideoTrack(track);
+  const event = await oneEvent(video.videoTracks, 'removetrack');
+  assert.equal(track, event.track);
+  assert.equal(video.videoTracks.length, 0);
+});
+
+it('fires queued addrendition event on selected track', async function () {
+  const video = await fixture(`<video></video>`);
+  const track = video.addVideoTrack('main');
+  track.selected = true;
+  const rendtion = track.addRendition('http://', 1920, 1080);
+  const event = await oneEvent(video.videoRenditions, 'addrendition');
+  assert.equal(rendtion, event.rendition);
+  assert.equal(video.videoRenditions.length, 1);
+});
+
+it('fires no addrendition event on unselected track', async function () {
+  const video = await fixture(`<video></video>`);
+  const track = video.addVideoTrack('main');
+
+  track.addRendition('http://', 1920, 1080);
+  await Promise.race([
+    oneEvent(video.videoRenditions, 'addrendition'),
+    aTimeout(200)
+  ]);
+
+  assert.equal(video.videoRenditions.length, 0);
 });
--- test/web-test-runner.config.js
+++ test/web-test-runner.config.js
@@ -1,4 +1,8 @@
 export default {
   nodeResolve: true,
-  files: ['test/**/*.js']
-};
+  files: ['test/**/*.js'],
+  coverageConfig: {
+    report: true,
+    include: ['dist/**/*'],
+  },
+}
